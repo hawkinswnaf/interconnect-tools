0c014adedd7c79ac38a3c5445cf449e1d926476a
Will Hawkins <hawkinsw@opentechinstitute.org>
2014-05-27 20:34:14 -0400

    Initial commit.

---
+++ run-rtts.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+months='01 04'
+sites='lax01-38.98 dfw01-38.107 sea01-38.102'
+for siteconfig in $sites; do
+	site=${siteconfig%%-*}
+	ip=${siteconfig##*-}
+	iplen=${#ip}
+
+	echo $site
+	echo $ip
+	echo $iplen
+
+	for month in $months; do
+		sql=`m4 << END
+select 
+HOUR(SEC_TO_TIMESTAMP(log_time)) as time,
+AVG(paris_traceroute_hop.rtt) as rtt
+from 
+m_lab.2014_MONTH
+where
+project = 3
+AND LEFT(connection_spec.server_ip,SERVERIPLEN) = "SERVERIP"
+AND (include(input/cogent.hops.src.input))
+AND (include(input/comcast.hops.dest.input))
+group by time
+order by time
+END`
+		query=`sed -e s/MONTH/$month/g -e s/SERVERIPLEN/$iplen/g -e s/SERVERIP/$ip/g <<< $sql`
+		bq query --format csv  $query | tee output/output-$site-$month-rtts.txt
+	done
+done
